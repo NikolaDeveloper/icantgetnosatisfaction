f9efe3d2491be24a0575cee71dd461ed31be0b33
Matthew Gambell <matt@gsproductionsmedia.com>
2016-03-11 16:32:31 -0700

    Updated Player stats and added other assets to train.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public static PlayerStats instance;
+
+	public string trainLineName = "";
+	public int playerMoney = 500;
+	public int satisfaction = 50;
+	public Color mainColor = Color.white;
+	public Color secondaryColor = Color.white;
+	public Color tertiaryColor = Color.white;
+
+
+
+	public PlayerStats(){
+		
+		if (instance == null) {
+			instance = this;
+		}
+
+		return instance;
+	}
+
+
+
+
+
+}
