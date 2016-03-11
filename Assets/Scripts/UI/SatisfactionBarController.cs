cdb628589eeb352cab24039b83b323dfc4c84c57
Matthew Gambell <matt@gsproductionsmedia.com>
2016-03-11 10:46:47 -0700

    Added Satisfaction UI Bar

---
+++ Assets/Scripts/UI/SatisfactionBarController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SatisfactionBarController : MonoBehaviour {
+
+	[Range(0,100)]
+	public int satisfaction;
+
+	public Image barFill;
+	public Text progressText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float satFloat = (float)satisfaction;
+		barFill.transform.localScale = new Vector3 (1, satFloat / 100f);
+
+		progressText.text = satFloat + "%";
+	}
+}
