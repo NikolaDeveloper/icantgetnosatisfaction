90539a1e5715a916f95f1af481ebb1042e3da810
NatalyCreates <nataly@natalycreates.com>
2016-03-11 22:42:55 +0200

    detect obstacle hits, find free lane WIP

--- Assets/Scripts/ObstacleHitter.cs
+++ Assets/Scripts/ObstacleHitter.cs
@@ -4,7 +4,29 @@ using System.Collections;
 public class ObstacleHitter : MonoBehaviour {
 
 
-	void Start()
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "block" || col.tag == "wood")
+        {
+            
+            // find free lane
+
+            foreach (ProcGen.Element e in ProcGen.Instance.topTrack)
+            {
+                float trainBackEdge = gameObject.transform.position.x - 75f;
+                float trainFrontEdge = gameObject.transform.position.x + 75f;
+                // check if tile is within the trains positions
+                if (e.pos + 25f >= trainBackEdge || e.pos - 25f <= trainFrontEdge)
+                {
+
+                }
+            }
+
+        }
+    }
+
+
+    void Start()
     {
 	
 	}
