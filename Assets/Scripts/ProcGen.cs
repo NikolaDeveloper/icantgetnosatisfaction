74a7515055de49177ece45c70500ab3b66cfecf2
NatalyCreates <nataly@natalycreates.com>
2016-03-11 02:21:10 +0200

    add a basic sprite and a script

---
+++ Assets/Scripts/ProcGen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProcGen : MonoBehaviour {
+
+    public static ProcGen Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
