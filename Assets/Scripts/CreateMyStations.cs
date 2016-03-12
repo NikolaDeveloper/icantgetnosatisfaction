42d3ba73a0263855b18e5dfd94c46459bb1074a4
NatalyCreates <nataly@natalycreates.com>
2016-03-12 03:59:05 +0200

    station proc gen

---
+++ Assets/Scripts/CreateMyStations.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMyStations : MonoBehaviour {
+
+    void Start()
+    {
+        int sLen = Random.Range(4, 8);
+        for (int i = 0; i < sLen; i++)
+        {
+            GameObject stationStripe;
+            int windows = Random.Range(0, 2);
+            if (windows == 0) stationStripe = Instantiate(ProcGen.Instance.staionStripePrefab, new Vector2(gameObject.transform.position.x + i * ProcGen.Instance.oneUnit, gameObject.transform.position.y), Quaternion.identity) as GameObject;
+            else stationStripe = Instantiate(ProcGen.Instance.stationStripeWithWindowPrefab, new Vector2(gameObject.transform.position.x + i * ProcGen.Instance.oneUnit, gameObject.transform.position.y), Quaternion.identity) as GameObject;
+            stationStripe.transform.parent = gameObject.transform;
+        }
+
+        gameObject.GetComponent<BoxCollider2D>().size = new Vector2((sLen - 1) * ProcGen.Instance.oneUnit, 450f);
+        gameObject.GetComponent<BoxCollider2D>().offset = new Vector2((sLen - 1) * (ProcGen.Instance.oneUnit / 2f), -25f);
+    }
+}
