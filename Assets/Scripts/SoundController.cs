9621257b005fde2c0608da141153612c254ab5c7
NatalyCreates <nataly@natalycreates.com>
2016-03-12 11:24:25 -0800

    added more placeholder sounds and stuff to sound controller, add another audio source to main camera prefab

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -5,9 +5,9 @@ public class SoundController : MonoBehaviour {
 
     public static SoundController Instance;
 
-    public AudioClip hitObstacleClip, stopStationClip;
+    public AudioClip hitObstacleClip, stopStationClip, moneyGainClip, screechBrake;
 
-    AudioSource musicObj;
+    AudioSource musicObj, trainLoopObj;
 
     float sfvol, mvol;
 
@@ -21,14 +21,41 @@ public class SoundController : MonoBehaviour {
 
     void Start()
     {
-        musicObj = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        musicObj = gameObject.GetComponents<AudioSource>()[0];
+        trainLoopObj = gameObject.GetComponents<AudioSource>()[1];
         musicObj.volume = mvol;
+        trainLoopObj.volume = sfvol;
     }
 
-	void Update()
+    public void MusicON()
     {
-	
-	}
+        musicObj.Play();
+    }
+
+    public void MusicOFF()
+    {
+        musicObj.Stop();
+    }
+
+    public void TrainMovingSoundON()
+    {
+        trainLoopObj.Play();
+    }
+
+    public void TrainMovingSoundOFF()
+    {
+        trainLoopObj.Stop();
+    }
+
+    public void PlayScreechBrakeSound()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(screechBrake, sfvol);
+    }
+
+    public void PlayMoneyGainSound()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(moneyGainClip, sfvol);
+    }
 
     public void PlayObstacleHitSound()
     {
