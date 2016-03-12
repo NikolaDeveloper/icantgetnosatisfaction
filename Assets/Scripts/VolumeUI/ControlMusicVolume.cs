7d3db0a8e3d08e06dfde666c40560b63fde271c7
NatalyCreates <nataly@natalycreates.com>
2016-03-12 10:34:25 -0800

    volume control UI

---
+++ Assets/Scripts/VolumeUI/ControlMusicVolume.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ControlMusicVolume : MonoBehaviour {
+
+    float mvol;
+
+    Slider slider;
+
+    void Awake()
+    {
+        mvol = PlayerPrefs.GetFloat("SatisfactionSettings_musicVolume", 0.8f);
+
+        slider = gameObject.GetComponentInChildren<Slider>();
+        slider.value = mvol;
+    }
+
+    public void VolumeSet()
+    {
+        mvol = slider.value;
+        PlayerPrefs.SetFloat("SatisfactionSettings_musicVolume", mvol);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/VolumeUI/ControlSFXVolume.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ControlSFXVolume : MonoBehaviour {
+
+    float sfvol;
+
+    Slider slider;
+
+    void Awake()
+    {
+        sfvol = PlayerPrefs.GetFloat("SatisfactionSettings_sfxVolume", 0.9f);
+
+        slider = gameObject.GetComponentInChildren<Slider>();
+        slider.value = sfvol;
+    }
+
+    public void VolumeSet()
+    {
+        sfvol = slider.value;
+        PlayerPrefs.SetFloat("SatisfactionSettings_sfxVolume", sfvol);
+    }
+
+    void Start()
+    {
+        
+    }
+}
