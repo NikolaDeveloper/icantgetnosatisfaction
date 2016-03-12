72c47daf34c22f965b77053398d959dc3597cc76
NatalyCreates <nataly@natalycreates.com>
2016-03-12 13:57:29 -0800

    added settings for main menu to save color and name WIP

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -16,10 +16,6 @@ public class Settings : MonoBehaviour {
 
     internal int gameDifficultyLevel = 1;
 
-    internal Color mainCol, stripeCol, windowsCol;
-
-    internal string trainName;
-
     void Awake()
     {
         Instance = this;
@@ -41,42 +37,11 @@ public class Settings : MonoBehaviour {
         // Load
 
         gameDifficultyLevel = PlayerPrefs.GetInt("SatisfactionSettings_gameDifficultyLevel", 1);
-
-        mainCol = new Color(PlayerPrefs.GetFloat("SatisfactionSettings_mainR", 1.0f), PlayerPrefs.GetFloat("SatisfactionSettings_mainG", 1.0f), PlayerPrefs.GetFloat("SatisfactionSettings_mainB", 1.0f));
-        stripeCol = new Color(PlayerPrefs.GetFloat("SatisfactionSettings_stripeR", 1.0f), PlayerPrefs.GetFloat("SatisfactionSettings_stripeG", 1.0f), PlayerPrefs.GetFloat("SatisfactionSettings_stripeB", 1.0f));
-        windowsCol = new Color(PlayerPrefs.GetFloat("SatisfactionSettings_windowsR", 1.0f), PlayerPrefs.GetFloat("SatisfactionSettings_windowsG", 1.0f), PlayerPrefs.GetFloat("SatisfactionSettings_windowsB", 1.0f));
-
-        trainName = PlayerPrefs.GetString("SatisfactionSettings_trainName", "");
-    }
-
-    void SetTrainName(string name)
-    {
-        trainName = name;
-        PlayerPrefs.SetString("SatisfactionSettings_trainName", trainName);
     }
 
     void SetDifficultyLevel(int setDiffLevel)
     {
         gameDifficultyLevel = setDiffLevel;
         PlayerPrefs.SetInt("SatisfactionSettings_gameDifficultyLevel", gameDifficultyLevel);
-    }
-
-    void SetTrainColor(Color main, Color stripe, Color windows)
-    {
-        mainCol = main;
-        stripeCol = stripe;
-        windowsCol = windows;
-
-        PlayerPrefs.SetFloat("SatisfactionSettings_mainR", mainCol.r);
-        PlayerPrefs.SetFloat("SatisfactionSettings_mainG", mainCol.g);
-        PlayerPrefs.SetFloat("SatisfactionSettings_mainB", mainCol.b);
-
-        PlayerPrefs.SetFloat("SatisfactionSettings_stripeR", stripeCol.r);
-        PlayerPrefs.SetFloat("SatisfactionSettings_stripeG", stripeCol.g);
-        PlayerPrefs.SetFloat("SatisfactionSettings_stripeB", stripeCol.b);
-
-        PlayerPrefs.SetFloat("SatisfactionSettings_windowsR", windowsCol.r);
-        PlayerPrefs.SetFloat("SatisfactionSettings_windowsG", windowsCol.g);
-        PlayerPrefs.SetFloat("SatisfactionSettings_windowsB", windowsCol.b);
     }
 }
