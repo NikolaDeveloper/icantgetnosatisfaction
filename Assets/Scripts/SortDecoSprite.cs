1f11e1e516d13352ef3f8956a4a774710eaa9db9
NatalyCreates <nataly@natalycreates.com>
2016-03-12 06:27:54 +0200

    deco proc gen and all prefabs with sorting by y value

---
+++ Assets/Scripts/SortDecoSprite.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortDecoSprite : MonoBehaviour {
+    
+	void Update()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y) * -1;
+    }
+}
